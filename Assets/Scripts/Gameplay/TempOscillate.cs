58ece13bea56917811465710fd3a0aa4c13d2066
Elena Angelica Lopez <elopez2585@floridapoly.edu>
2018-11-13 18:09:49 -0500

    sprites
    - removed old sprites
    - added most new sprites (need to figure out how to do animated tiles still)
    - test scene in unity using new sprites/temp script

---
+++ Assets/Scripts/Gameplay/TempOscillate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TempOscillate : NetworkBehaviour {
+
+    [SerializeField]
+    private float delta = 1.5f;  // Amount to move left and right from the start point
+    [SerializeField]
+    private float speed = 2.0f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        if (isServer)
+        {
+            startPos = transform.position;
+            StartCoroutine(slowUpdate());
+        }
+
+    }
+
+    [Server]
+    public IEnumerator slowUpdate()
+    {
+        while (true)
+        {
+            Vector3 v = startPos;
+            v.x += delta * Mathf.Sin(Time.time * speed);
+            transform.position = v;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+	
+}
