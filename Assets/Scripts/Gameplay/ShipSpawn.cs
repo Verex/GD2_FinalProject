496dd639915bb9e165570c632c633a35c0d2fbfa
Zach <zach.verex@gmail.com>
2018-11-14 21:06:10 -0500

    Added player spawn locations.

---
+++ Assets/Scripts/Gameplay/ShipSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShipSpawn : NetworkBehaviour
+{
+	// Not to be confused with network player id (Spawning only).
+	[SerializeField] public int playerID;
+
+	public override void OnStartServer()
+	{
+		// Register this spawn.
+		NetworkHandler.Instance.RegisterShipSpawn(playerID, transform);
+	}
+}
