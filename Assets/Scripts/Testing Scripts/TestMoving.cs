9c9c0be5e20739a00da5fcff0dfa88bce5bbd5d1
Elena Angelica Lopez <elopez2585@floridapoly.edu>
2018-11-19 13:12:07 -0500

    sprites/level
    a lot of stuff

---
+++ Assets/Scripts/Testing Scripts/TestMoving.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoving : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.up * 0.04f);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+
+    }
+}
